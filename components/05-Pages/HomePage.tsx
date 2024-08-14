445019dfa6b21e03ee35f8fb780bc6679cbf648a
aslammaulana <aslammaulana1998@gmail.com>
2024-08-14 22:11:00 +0700

    1.5

--- components/05-Pages/HomePage.tsx
+++ components/05-Pages/HomePage.tsx
@@ -6,18 +6,18 @@ import TwoColumnLayout from '@/components/03-Organisms/TwoColumnLayout';
 export default function HomePage() {
     return (
         <Layout>
-            <div className="flex items-center justify-center bg-gray-100 p-4 lg:min-h-screen">
+            <div className=" flex items-center justify-center bg-gray-100 p-4 lg:min-h-screen">
                 <div className="grid max-w-3xl w-full h-auto rounded-xl bg-white overflow-hidden shadow-lg grid-cols-1 lg:grid-cols-11">
 
                     {/* Kolom Kanan: Welcome Panel */}
-                    <div className="lg:block lg:col-span-5 py-[10px] px-[10px]">
+                    <div className=" lg:block lg:col-span-5 py-[10px] px-[10px]">
                         <TwoColumnLayout />
                     </div>
-
                     {/* Kolom Kiri: Form Login */}
                     <div className="w-full lg:col-span-6 flex justify-center items-center p-[10px]">
                         <LoginForm />
                     </div>
+
                 </div>
             </div>
         </Layout>
